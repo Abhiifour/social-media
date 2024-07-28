8d18f7e310c38707183da14bee143ff4383b77c5
Abhiifour <as8998013@gmail.com>
2024-07-29 00:32:05 +0530

    update

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:3000'
+    origin:'https://social-media-flax-seven.vercel.app/'
 }))
 
 app.use('/auth' , authRouter);
